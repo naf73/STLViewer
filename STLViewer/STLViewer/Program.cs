753e85c1010aca15f768006e5fbfc114b09c93b8
naf73 <alexnejchev73@gmail.com>
2017-06-24 22:42:19 +0400

    add verefy repeated run apps

--- STLViewer/STLViewer/Program.cs
+++ STLViewer/STLViewer/Program.cs
@@ -3,20 +3,48 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Runtime.InteropServices;
+using System.Threading;
+using System.Diagnostics;
 
 namespace STLViewer
 {
     static class Program
     {
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        static extern bool SetForegroundWindow(IntPtr hWnd);
         /// <summary>
         /// Главная точка входа для приложения.
         /// </summary>
         [STAThread]
         static void Main(string[] args)
         {
-            Application.EnableVisualStyles();
-            Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new FormMain(args));
+            bool createdNew = true;
+            using (Mutex mutex = new Mutex(true, "STLViewer", out createdNew))
+            {
+                if (createdNew)
+                {
+                    Application.EnableVisualStyles();
+                    Application.SetCompatibleTextRenderingDefault(false);
+                    Application.Run(new FormMain(args));
+                }
+                else
+                {
+                    Process current = Process.GetCurrentProcess();
+
+                    foreach (Process process in Process.GetProcessesByName(current.ProcessName))
+                    {
+                        if (process.Id != current.Id)
+                        {
+                            SetForegroundWindow(process.MainWindowHandle);
+                            MessageBox.Show("Программа уже запущена!!!", "STLViewer", MessageBoxButtons.OK, MessageBoxIcon.Stop);
+                            break;
+                        }
+                    }
+                }
+            }
         }
     }
 }
